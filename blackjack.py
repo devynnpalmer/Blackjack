bd364e7bdd996f039f611bdc48753e9ffc287f67
devynnpalmer <palmerdevynn@gmail.com>
2017-07-04 19:54:58 -0400

    fixed!

--- blackjack.py
+++ blackjack.py
@@ -15,10 +15,14 @@ card_frame.grid(row=1, column=0, sticky='ew', columnspan=3, rowspan=2)
 
 dealer_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Dealer", background="green", fg='white').grid(row=0, column=0)
-tkinter.Label(card_frame, text=dealer_score_label, background="green")
+tkinter.Label(card_frame, textvariable=dealer_score_label, background="green")
 
 dealer_card_frame = tkinter.Frame(card_frame, background="green")
 dealer_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
+
+player_score_label = tkinter.IntVar()
+tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
+tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=2, column=0)
 
 
 
