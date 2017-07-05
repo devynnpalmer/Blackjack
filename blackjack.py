d7a7e36414d665fbed474594f242a3b36be29bde
devynnpalmer <palmerdevynn@gmail.com>
2017-07-04 20:11:14 -0400

    buttonz

--- blackjack.py
+++ blackjack.py
@@ -24,5 +24,12 @@ player_score_label = tkinter.IntVar()
 tkinter.Label(card_frame, text="Player", background="green", fg="white").grid(row=2, column=0)
 tkinter.Label(card_frame, textvariable=player_score_label, background="green", fg="white").grid(row=2, column=0)
 
+button_frame = tkinter.Frame(mainWindow)
+button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
 
+dealer_button = tkinter.Button(button_frame, text="Dealer")
+dealer_button.grid(row=0, column=0)
+
+player_button = tkinter.Button(button_frame, text="Player")
+player_button.grid(row=0, column=1)
 
