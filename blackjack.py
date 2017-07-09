d371d9ee3bb57e8008ab9d50b085e12538558ecc
devynnpalmer <palmerdevynn@gmail.com>
2017-07-08 20:50:52 -0400

    it's alive!

--- blackjack.py
+++ blackjack.py
@@ -20,20 +20,23 @@ def load_images(card_images):
         for card in range(1, 11):
             name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
             image = tkinter.PhotoImage(file=name)
-            card_images.append(card, image,)
+            card_images.append((card, image,))
 
         for card in face_cards:
             name = 'cards/{}_{}.{}'.format(str(card), suit, extension)
             image = tkinter.PhotoImage(file=name)
-            card_images.append(10, image,)
+            card_images.append((10, image,))
+
 
 def deal_card(frame):
     next_card = deck.pop()
     tkinter.Label(frame, image=next_card[1], relief='raised').pack(side='left')
     return next_card
 
+
 def deal_dealer():
     deal_card(dealer_card_frame)
+
 
 def deal_player():
     deal_card(player_card_frame)
@@ -44,6 +47,7 @@ mainWindow.geometry("640x480")
 
 result_text = tkinter.StringVar()
 result = tkinter.Label(mainWindow, textvariable=result_text)
+result.grid(row=0, column=0,columnspan=3)
 
 card_frame = tkinter.Frame(mainWindow, relief="sunken", borderwidth=1, background="green")
 card_frame.grid(row=1, column=0, sticky='ew', columnspan=3, rowspan=2)
@@ -62,14 +66,13 @@ tkinter.Label(card_frame, textvariable=player_score_label, background="green", f
 player_card_frame = tkinter.Frame(card_frame, background="green")
 player_card_frame.grid(row=0, column=1, sticky="ew", rowspan=2)
 
-
 button_frame = tkinter.Frame(mainWindow)
 button_frame.grid(row=3, column=0, columnspan=3, sticky='w')
 
-dealer_button = tkinter.Button(button_frame, text="Dealer", command=deal_card(dealer_card_frame))
+dealer_button = tkinter.Button(button_frame, text="Dealer", command=deal_dealer)
 dealer_button.grid(row=0, column=0)
 
-player_button = tkinter.Button(button_frame, text="Player")
+player_button = tkinter.Button(button_frame, text="Player", command=deal_player)
 player_button.grid(row=0, column=1)
 
 cards = []
